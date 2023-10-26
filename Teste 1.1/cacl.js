bd2d47c5e84edcccea6dbdd3e63c3c9af0532092
DevJorgeTech <128604408+DevJorgeTech@users.noreply.github.com>
2023-10-26 16:22:17 -0300

    Calculadora versão 1.1
    Melhorando o CSS e iniciando com o JavaScript.

---
+++ Teste 1.1/cacl.js
@@ -0,0 +1,18 @@
+const delet = document.getElementById('delete')
+const result = document.getElementById('result')
+delet == 0
+num1 == 0
+num2 == 0
+op == 0
+
+
+function cacl(num1){
+    result.innerHTML = num1
+    return num1
+}
+
+function cacl2(num2){
+    if (op != 0){
+        result.innerHTML = num2
+    }
+}
